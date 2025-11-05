48d936ec1888f1dcc55c3fdf6e0eaf6b32e4aed4
Abdelkader Boudih <terminale@gmail.com>
2025-11-05 07:47:21 +0100

    Support TruffleRuby with native thread-based executor

--- lib/solid_queue/java/executor.rb
+++ lib/solid_queue/java/executor.rb
@@ -2,36 +2,84 @@
 
 module SolidQueue
   module Java
-    # Java ExecutorService-based worker pool implementation
+    # Worker pool implementation - uses Java ExecutorService on JRuby, Threads on TruffleRuby
     class Executor
-      java_import java.util.concurrent.Executors
-      java_import java.util.concurrent.TimeUnit
+      if RUBY_ENGINE == 'jruby'
+        java_import java.util.concurrent.Executors
+        java_import java.util.concurrent.TimeUnit
 
-      attr_reader :executor_service, :worker_count
+        attr_reader :executor_service, :worker_count
 
-      def initialize(worker_count = 4)
-        @worker_count = worker_count
-        @executor_service = Executors.newFixedThreadPool(worker_count)
-        @futures = []
-      end
+        def initialize(worker_count = 4)
+          @worker_count = worker_count
+          @executor_service = Executors.newFixedThreadPool(worker_count)
+          @futures = []
+        end
 
-      def submit(&block)
-        future = @executor_service.submit(block)
-        @futures << future
-        future
-      end
+        def submit(&block)
+          future = @executor_service.submit(block)
+          @futures << future
+          future
+        end
 
-      def shutdown
-        @executor_service.shutdown
-        @executor_service.awaitTermination(30, TimeUnit::SECONDS)
-      end
+        def shutdown
+          @executor_service.shutdown
+          @executor_service.awaitTermination(30, TimeUnit::SECONDS)
+        end
 
-      def shutdown_now
-        @executor_service.shutdownNow
-      end
+        def shutdown_now
+          @executor_service.shutdownNow
+        end
 
-      def wait_for_all
-        @executor_service.awaitTermination(Integer::MAX_VALUE, TimeUnit::SECONDS)
+        def wait_for_all
+          @executor_service.awaitTermination(Integer::MAX_VALUE, TimeUnit::SECONDS)
+        end
+      else
+        # TruffleRuby: use native threads
+        attr_reader :worker_count, :threads
+
+        def executor_service
+          self
+        end
+
+        def initialize(worker_count = 4)
+          @worker_count = worker_count
+          @threads = []
+          @queue = ::Queue.new
+          @shutdown = false
+
+          worker_count.times do
+            @threads << Thread.new { run_worker }
+          end
+        end
+
+        def submit(&block)
+          @queue.push(block)
+        end
+
+        def shutdown
+          shutdown_now
+          @threads.each(&:join)
+        end
+
+        def shutdown_now
+          @shutdown = true
+          @queue.close
+        end
+
+        def wait_for_all
+          @threads.each(&:join)
+        end
+
+        private
+
+        def run_worker
+          while (work = @queue.pop)
+            work.call
+          end
+        rescue ClosedQueueError
+          # Queue closed, thread exits
+        end
       end
     end
   end
